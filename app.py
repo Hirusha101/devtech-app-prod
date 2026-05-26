fbdd2def61293e63ba2fbfe0fe20868373b0608d
Hirusha101 <157349624+Hirusha101@users.noreply.github.com>
2026-05-26 19:59:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 import os
 import requests # Simulate using an unpatched, vulnerable library version
 
-# SECURITY DEFECT ATTEMPT: Hardcoded Administrative Credential (Secret Leak Token)
-AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE_FAKE_KEY_DO_NOT_USE"
+
 
 def main():
     print("Software Application Core Services Starting...")
